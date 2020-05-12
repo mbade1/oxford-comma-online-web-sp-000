ec248b4594d0be79d35159b298328acc237dadb3
Michael Bade <mbade314@gmail.com>
2020-05-12 01:31:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
   if array.length == 1
-    return array.join
-    return joined
+    return "#{array[0]}"
   elsif array.length == 2
     joined = array.join(" and ")
     return joined
   elsif array.length == 3
     joined = array.join(", ")
+    puts joined + 
     return joined
   end
 end
