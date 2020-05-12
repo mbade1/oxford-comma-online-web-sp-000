8c7538e51f006c6244b07faba24fa6ce4758b79d
Michael Bade <mbade314@gmail.com>
2020-05-12 01:33:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    joined = array.join(" and ")
+    return joined
+  elsif array.length > 2
+    last_item = "and #{array.last}"
+    array.pop
+    array.push(last_item)
+    return array.join(", ")
+  end
 end
